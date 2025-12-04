6c84d29a2b1f6b6d22a7f80cf544f76bce5bd79f
Ali Ghimar Budiana <alighimar2040@gmail.com>
2025-12-04 16:11:41 +0700

    Update

--- Modul12/Jurnal01.java
+++ Modul12/Jurnal01.java
@@ -1,6 +1,31 @@
+import java.util.*;
 public class Jurnal01 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+        Scanner in = new Scanner(System.in);
+        int barisKolom = in.nextInt();
+        
+        int[][] array = new int[barisKolom][barisKolom];
+        
+        for (int i = 0; i < barisKolom; i++) {
+            for (int j = 0; j < barisKolom; j++) {
+                array[i][j] = in.nextInt();
+            }
+        }
+        
+        for (int i = 0; i < barisKolom / 2; i++) {
+            int[] ali = array[i];
+            array[i] = array[barisKolom - 1 - i];
+            array[barisKolom - 1 - i] = ali;
+        }
+        
+        for (int i = 0; i < barisKolom; i++) {
+            for (int j = 0; j < barisKolom; j++) {
+                System.out.print(array[i][j]);
+                if (j < barisKolom - 1)
+                System.out.print(" ");   
+            }
+            System.out.println();
+        }
     }
 }
--- Modul12/Jurnal02.java
+++ Modul12/Jurnal02.java
@@ -1,6 +1,92 @@
+import java.util.Scanner;
 public class Jurnal02 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+        Scanner sc = new Scanner(System.in);
+        int inputan = sc.nextInt();
+        int [][] papan = new int[inputan][inputan];
+
+        boolean tictactoe = false;
+
+        // input papan
+        for (int i = 0; i < inputan; i++) {
+            for (int j = 0; j < inputan; j++) {
+                papan[i][j] = sc.nextInt();
+                if (papan[i][j] == 0){
+                    tictactoe = true;
+                }
+            }
+        }
+
+        // cek baris
+        for (int i = 0; i < inputan; i++){
+            int baris = papan[i][0];
+            if (baris != 0) {
+                boolean sama = true;
+                for (int j = 1; j < inputan; j++){
+                    if (papan[i][j] != baris){
+                        sama = false;
+                        break;
+                    }
+                }
+                if (sama) {
+                    System.out.println(baris == 1 ?  "O" : "X");
+                    return;
+                }
+            }
+        }
+
+        // cek kolom
+        for (int j = 0; j < inputan; j++) {
+            int kolom = papan[0][j];
+            if (kolom != 0) {
+                boolean sama = true;
+                for (int i = 1; i < inputan; i++){
+                    if (papan[i][j] != kolom){
+                        sama = false;
+                        break;
+                    } 
+                }
+                if (sama) {
+                    System.out.println(kolom == 1 ? "O" : "X");
+                    return;
+                }
+            }
+        }
+
+        // diagonal utama
+        int diag = papan[0][0];
+        if (diag != 0){
+            boolean sama = true;
+            for (int i = 1; i < inputan; i++){
+                if (papan[i][i] != diag) {
+                    sama = false;
+                    break;
+                }
+            }
+            if (sama) {
+                System.out.println(diag == 1 ? "O" : "X");
+                return;
+            }
+        }
+
+        // diagonal kedua
+        diag = papan[0][inputan - 1];
+        if (diag != 0) {
+            boolean sama = true;
+            for (int i = 1; i < inputan; i++) {
+                if (papan[i][inputan - 1 - i] != diag){
+                    sama = false;
+                    break;
+                }
+            }
+            if (sama) {
+                System.out.println(diag == 1 ? "O" : "X");
+                return;
+            }
+        }
+
+        // tidak ada pemenang
+        System.out.println("?");
     }
 }
